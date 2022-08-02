6799d1c486b71b2b262fad6d34bdf405359ef7fb
Jin Choi <91814116+JinDevlp@users.noreply.github.com>
2022-08-02 16:43:49 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+data = pandas.read_csv(
+    "/Users/jinchoi/Desktop/python_github/list_comprehension.py/nato_phonetic_alphabet.csv")
+
+new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(new_dict)
+
+
+word = input("Enter a word: ").upper()
+output_list = [new_dict[letter]for letter in word]
+print(output_list)
